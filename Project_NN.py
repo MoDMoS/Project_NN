ffd25886aa16abb566c0e47b6b87585023953560
MoD_MoS <koonmos1@gmail.com>
2022-11-12 15:52:08 +0700

    Fix age perdict

--- Project_NN.py
+++ Project_NN.py
@@ -16,21 +16,40 @@ model_A = keras.models.load_model('Model_Age.h5')
 
 # Define the prediction function
 def predict(uploaded_file):
-    if uploaded_file is not None:
-        img = Image.open(uploaded_file)
-        img = img.resize((48,48),Image.ANTIALIAS)
-        img = np.float32(img)
-        (R, G, B) = cv2.split(img)
-        G = G.reshape(-1, 48, 48, 1)
+    img = cv2.imread(uploaded_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('/haarcascade_frontalface_alt2.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-        result_G = model_G.predict(G)
-        result_A = model_A.predict(G)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        faces = img[y:y + h, x:x + w]
+    if(faces == ()) :
+        return "Fail"
+    else :
+        if uploaded_file is not None:
+            img = Image.open(uploaded_file)
+            img = img.resize((48,48),Image.ANTIALIAS)
+            img = np.float32(img)
+            (R, G, B) = cv2.split(img)
+            G = G.reshape(-1, 48, 48, 1)
 
-        if(round(result_G[0][0])!=0) :
-            result_G = "Female"
-        else :
-            result_G = "Male"
-    return result_G, int(result_A[0][0])
+            result_G = model_G.predict(G)
+
+            if(round(result_G[0][0])!=0) :
+                result_G = "Female"
+            else :
+                result_G = "Male"
+            
+            img = Image.fromarray(faces)
+            img = img.resize((48,48),Image.ANTIALIAS)
+            img = np.float32(img)
+            (R, G, B) = cv2.split(img)
+            G = G.reshape(-1, 48, 48, 1)
+
+            result_A = model_A.predict(G)
+
+        return result_G, int(result_A[0][0])
 
 st.title('Age & Gender perdiction')
 st.header('Enter image for perdiction:')
@@ -40,5 +59,8 @@ if uploaded_file is not None:
 
 
 if st.button('Predict Price'):
-    price = predict(uploaded_file)
-    st.text("Gender : " + price[0] + '\nAge : ' + str(price[1]))
+    Predict = predict(uploaded_file)
+    if(Predict == "Fail") :
+        st.text("Can't detect face")
+    else :
+        st.text("Gender : " + Predict[0] + '\nAge : ' + str(Predict[1]))
